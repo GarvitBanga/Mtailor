8e59ecaa94023caa2b3054cc6d5ca1a9804da8c6
Garvit Banga <bangaboy181@gmail.com>
2025-06-06 23:49:12 -0400

    Onnx Model loading and prediction call

--- model.py
+++ model.py
@@ -1,4 +1,6 @@
 import numpy as np
+import onnxruntime
+from PIL import Image
 
 
 class ImgPreProcessing:
@@ -14,3 +16,23 @@ class ImgPreProcessing:
         img=(img-self.mean)/self.std
         img=np.transpose(img,(2,0,1))
         img=np.expand_dims(img,axis=0)
+        return img
+
+class ONNX:
+    def __init__(self,path):
+        self.inference_session=onnxruntime.InferenceSession(path)
+        self.input_name=self.inference_session.get_inputs()[0].name
+
+    def predict(self,img):
+        img=img.astype(np.float32)
+        prediction=self.inference_session.run(None,{self.input_name:img})
+        return int(np.argmax(prediction[0]))
+
+if __name__ == "__main__":
+    preprocessing=ImgPreProcessing()
+    onnx=ONNX("resnet.onnx")
+    test_images=["n01440764_tench.jpeg","n01667114_mud_turtle.JPEG"]
+    for path in test_images:
+        img=Image.open(path)
+        img=preprocessing.preprocess(img)
+        print(onnx.predict(img))
